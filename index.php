9c6bcefcd59d60361c836182ea070888311bc172
EtewaZINSOU <etewa.zinsou@gmail.com>
2015-02-08 17:45:55 +0100

    Ajout du code dans ce fichier

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+$loi=array(
+   " Un robot ne peut porter atteinte à un être humain, ni, en restant passif, permettre qu'un être humain soit exposé au danger.",
+   "Un robot doit obéir aux ordres que lui donne un être humain, sauf si de tels ordres entrent en conflit avec la Première loi.",
+   "Un robot doit protéger son existence tant que cette protection n'entre pas en conflit avec la Première ou la Deuxième loi."
+);
+
